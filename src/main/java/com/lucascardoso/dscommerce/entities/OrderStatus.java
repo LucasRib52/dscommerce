a50dfc4a146dda175407484c71d4065f42519d74
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-01-29 07:11:33 -0300

    feat: add User and Order/ configuration h2-test-database

---
+++ src/main/java/com/lucascardoso/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.lucascardoso.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
---
+++ src/test/java/com/lucascardoso/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascardoso.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
