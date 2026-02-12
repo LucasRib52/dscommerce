9cfe033a5a68a401dd42a1fe23c24d307eaf8c4d
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-02-12 11:23:20 -0300

    Feat: add Bean validation and exceptions

---
+++ src/main/java/com/lucascardoso/dscommerce/dto/FieldMessage.java
@@ -0,0 +1,23 @@
+package com.lucascardoso.dscommerce.dto;
+
+public class FieldMessage {
+
+    private String fildName;
+    private String message;
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String fildName, String message) {
+        this.fildName = fildName;
+        this.message = message;
+    }
+
+    public String getFildName() {
+        return fildName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
