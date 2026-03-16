fed0ea1fa168f67823f867b1766d89a7763b5a7d
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-03-16 15:55:20 -0300

    ProductMinDTO consultar catalogo - findall

--- src/main/java/com/lucascardoso/dscommerce/services/ProductService.java
+++ src/main/java/com/lucascardoso/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.lucascardoso.dscommerce.services;
 
 import com.lucascardoso.dscommerce.dto.ProductDTO;
+import com.lucascardoso.dscommerce.dto.ProductMinDTO;
 import com.lucascardoso.dscommerce.entities.Product;
 import com.lucascardoso.dscommerce.repositories.ProductRepository;
 import com.lucascardoso.dscommerce.services.exceptions.DatabaseException;
@@ -28,9 +29,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
 
